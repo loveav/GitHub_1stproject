c242d26e73c551df4bda7d0dc223c4438e4ef18e
Madad Vineet <1691678@CEGEPGIM.LCL>
2017-05-05 11:19:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1691678/myapplication/GitHub.java
@@ -0,0 +1,13 @@
+package com.example.a1691678.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hub);
+    }
+}
